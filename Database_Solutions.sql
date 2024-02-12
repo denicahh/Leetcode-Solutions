680a313a08885b631a03c3608c97409d91aba5fb
Denica <denicah626@gmail.com>
2024-02-13 08:57:04 +1100

    Update Database_Solutions.sql

--- Database_Solutions.sql
+++ Database_Solutions.sql
@@ -11,4 +11,18 @@ FROM Employee e1
 JOIN Employee e2 ON e1.managerid = e2.id
 WHERE e1.salary > e2.salary;
 
+#182 Duplicate Emails 
+
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+#183 Customers Who Never_Order
+
+SELECT name AS Customers
+FROM Customers
+WHERE id NOT IN (SELECT customerID FROM Orders)
+
+
 
